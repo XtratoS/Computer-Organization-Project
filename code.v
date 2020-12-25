22251e80b8a2f6c470b5db462ecb72b7dae43274
XtratoS <ultimate.nuker@yahoo.com>
2020-12-25 15:23:43 +0200

    Updated after 3rd meeting on the 24th of December

--- code.v
+++ code.v
@@ -1,29 +1,11 @@
-module clockGen(Clock);
-    output Clock;
-    reg Clock;
+module Clock(clock);
+    output clock;
+    reg clock;
     initial
-        Clock = 0;
+        clock = 0;
     always
-        #5 Clock = ~Clock;
+        #5 clock = ~clock;
 endmodule
-
-// module clockTestBench;
-//     clockGen C(clk);
-//     initial begin
-//         $monitor(clk);
-//     end
-//     always @(posedge clk) begin
-//         if ($time > 40) begin
-//             $finish;
-//         end
-//     end
-// endmodule
-
-// module Target(Address, )
-
-//     parameter device_address = 32'h0000010;
-
-// endmodule
 
 // The Main Module
 module PCI(
@@ -32,73 +14,96 @@ module PCI(
     input IRDY,
     input [3:0] CBE,
     inout [31:0] AD,
-    output reg TRDY
+    output reg TRDY,
+    output reg DEVSEL
 );
-
-    reg DEVSEL = 1'b1;
-    reg READ_FLAG = 0;
-    reg CLOCK_COUNTER;
+    parameter PCI_read = 4'b0010;
+    parameter PCI_write = 4'b0011;
+    parameter device_address = 32'h0000010;
+    //Shows whether or not the device address matches what's on the ADDRESS LINE
+    wire TARGETED = (device_address == AD);
+    wire TRANSACTION = ~Frame;
+    wire LAST_DATA_READ = Frame && ~IRDY;
+    reg NEG_CLOCK_COUNTER;
+    reg DEVSEL_2 = 1'b1;
+    reg TRDY_2 = 1'b1;
+    reg [31:0] DATA_REG = 32'h1020304;
+    assign AD = ((CBE == PCI_read) && ~IRDY) ? DATA_REG : 32'hzzzzzzz;
+    // reg [1:0] INDEX = 0;
     
-    reg CHECK_ADDRESS = 0;
+    //memory belongs to the target
+    reg [31:0] MEMORY [0:3];
     
     // Reset(Frame, TRDY, IRDY, DEVSEL);
     
     // This device address imitates a device connected to the PCI, we check the provided address
     // against this one to determine if we will set DEV_SEL or not
-    parameter device_address = 32'h0000010;
+
     
     always @(negedge clk) begin
+        DEVSEL <= DEVSEL_2;
+        TRDY <= TRDY_2;
         // Increment the clock counter every negative edge of the clock in order to read it later in the positive edge of the clock
-        CLOCK_COUNTER <= CLOCK_COUNTER + 1;
-        if (READ_FLAG == 1) begin
-            if (AD == device_address) begin
-                #5 DEVSEL <= 0;
-                #5 TRDY <= 0;
-            end
+        NEG_CLOCK_COUNTER++;
+        // DEVSEL and TRDY fast
+        if (TRANSACTION || LAST_DATA_READ) begin
+            DEVSEL_2 <= 0;
+            TRDY_2 <= 0;
+        end
+        if (NEG_CLOCK_COUNTER == 2) begin
+            // DATA_REG <= 32'h1000220;
         end
     end
     
     always @(posedge clk) begin
         // Read the address at the 1st positive edge of the clock after the start of the frame
-        if (CLOCK_COUNTER == 1) begin
+        if (NEG_CLOCK_COUNTER == 1) begin
             //READ ADDRESS
+            if (TARGETED) begin
+                DEVSEL_2 <= 1;
+                TRDY_2 <= 1;
+            end
         // Turnaround in the 2nd positive edge of the clock after the start of the frame
-        end else if (CLOCK_COUNTER == 2) begin
+        end else if (NEG_CLOCK_COUNTER == 2) begin
             //TURNAROUND
+        end else begin
+            //WE PUT THE DATA ON THE ADDRESS LINE ON THE NEGATIVE EDGE OF THE CLOCK
         end
     end
     
     // Set the READ_FLAG at the start of the frame
     // Reset the counter at the start of the frame
-    always @(negedge Frame) begin
-        READ_FLAG <= 1;
-        CHECK_ADDRESS <= 1;
-        CLOCK_COUNTER <= 1;
-    end
+    // always @(negedge Frame) begin
+    //     READ_FLAG <= 1;
+    //     NEG_CLOCK_COUNTER = 1; //0
+    // end
     
     // Reset the READ_FLAG at the positive edge of the frame.
-    always @(posedge Frame) begin
-        #5 READ_FLAG <= 0;
-    end
+    // always @(posedge Frame) begin
+    // #5 READ_FLAG <= 0;
+    // end
     
 endmodule
 
 module PCITest;
+    parameter PCI_read = 4'b0010;
+    parameter PCI_write = 4'b0011;
     // Instantiating the clock
-    clockGen C(clk);
+    Clock C(clk);
     // Instantiation the PCI Variables
-    wire [31:0] AD = 32'h0000010;
+    wire [31:0] AD = ((CBE == PCI_read) && ~IRDY) ? 32'hzzzzzzz : 32'h0000010;
     reg Frame = 1;
-    reg IRDY = 0;
-    wire TRDY = 0;
-    reg [3:0]CBE = 4'b1111;
+    reg IRDY = 1;
+    reg [3:0] CBE = 4'b0010;
+    wire intermediate = ((CBE == PCI_read) && ~IRDY);
     // PCI Instance
-    PCI pci(clk, Frame, IRDY, CBE, AD, TRDY);
+    PCI pci(clk, Frame, IRDY, CBE, AD, TRDY, DEVSEL);
     initial begin
         // Monitor the frame and clock
-        $monitor(Frame, clk);
+        $monitor(Frame, " ", DEVSEL, " ", intermediate, " ", TRDY, AD, $time);
         // Set the frame to low after 15 nanosecond
         #15 Frame <= 0;
+        #10 IRDY <= 0;
     end
     // Always block at positive and negative edges of the clock
     always @(posedge clk, negedge clk)
